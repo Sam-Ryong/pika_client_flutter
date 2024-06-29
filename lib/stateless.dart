7fea80ae67bd1bb4f00ed267fd67670c6026ea74
sam_ryong <zackinthebox@khu.ac.kr>
2024-06-29 14:40:25 +0900

    expanded, context theme, flexible에 대해서 연습

---
+++ lib/stateless.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:pika_client_flutter/widgets/button.dart';
+import 'package:pika_client_flutter/widgets/card.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text("Hey, Samryong",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                            )),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 20),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Total Balance",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 2),
+                const Text(
+                  "\$5 194 382",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    MyButton(
+                      text: "Transfer",
+                      backgroundColor: Colors.amber,
+                      textColor: Colors.black,
+                    ),
+                    MyButton(
+                      text: "Request",
+                      backgroundColor: Color(0xFF1F2123),
+                      textColor: Colors.white,
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Wallets",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const MyCard(
+                  name: "Euro",
+                  balance: "6 428",
+                  index: "EUR",
+                  icon: Icons.euro_rounded,
+                  isInverted: false,
+                  order: 0,
+                ),
+                const MyCard(
+                  name: "Bitcoin",
+                  balance: "3.24",
+                  index: "BTC",
+                  icon: Icons.currency_bitcoin_rounded,
+                  isInverted: true,
+                  order: 1,
+                ),
+                const MyCard(
+                  name: "Dollar",
+                  balance: "100",
+                  index: "USD",
+                  icon: Icons.attach_money_rounded,
+                  isInverted: false,
+                  order: 2,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
