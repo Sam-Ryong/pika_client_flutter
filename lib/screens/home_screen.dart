d1be2850ed502e4b104a3612a82d327527e5416c
SamRyong <zackinthebox@khu.ac.kr>
2024-07-02 10:30:43 +0900

    listView 연습

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,49 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:pika_client_flutter/models/webtoon_model.dart';
 import 'package:pika_client_flutter/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
         title: const Text(
-          "오늘의 웹툰",
+          "어늘의 웹툰",
           style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w500,
+            fontSize: 24,
           ),
         ),
-        foregroundColor: Colors.green,
-        backgroundColor: Colors.white,
-        elevation: 2,
-        shadowColor: Colors.black,
-        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 200,
+                ),
+                makeList(snapshot)
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
+}
+
+ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+  return ListView.separated(
+    separatorBuilder: (context, index) {
+      return const SizedBox(
+        height: 10,
+      );
+    },
+    scrollDirection: Axis.vertical,
+    itemCount: snapshot.data!.length,
+    itemBuilder: (context, index) {
+      var webtoon = snapshot.data![index];
+      return Text(webtoon.title);
+    },
+  );
 }
